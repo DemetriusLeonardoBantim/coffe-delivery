fc07dca14caabc4a391dcc05536a1349f0f66bc3
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-08-12 18:20:03 +0000

    feature: create style to InfoLocation

---
+++ src/components/InfoLocation/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Content = styled.div`
+    max-width: 1120px;
+`
+export const Legend = styled.h1`
+    color: var(--purple-dark);
+    font-size: 16px;
+`
